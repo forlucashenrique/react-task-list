68cfb234f46aadb7bef609a03fa6725e60c96dd7
Lucas Henrique <forlucashenrique@gmail.com>
2022-08-05 10:13:29 -0300

    inicializando o projeto

---
+++ src/components/layout/ContainerTask.js
@@ -0,0 +1,7 @@
+export default function ContainerTask(){
+    return (
+        <div className='w-4/5 h-3/5 m-auto '>
+
+        </div>
+    )
+}
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer(){
+    return (
+        <div className='w-full flex justify-center pt-6'>
+            <footer>
+                <p className='text-slate-200'> &copy; made by Lucas Henrique </p>
+            </footer>
+        </div>
+    )
+}
